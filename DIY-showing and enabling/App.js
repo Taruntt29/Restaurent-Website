2a47083a41c4f54795bbc4be88e3e2b0b0bb11e5
Taruntt29 <109497779+Taruntt29@users.noreply.github.com>
2022-09-04 11:44:09 +0530

    Add files via upload

---
+++ DIY-showing and enabling/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import CartProvider from './store/CartProvider';
+import Header from './components/Layout/Header';
+import MealsSummary from './components/Meals/MealsSummary';
+import AvailableMeals from './components/Meals/AvailableMeals';
+import Cart from './components/Cart/Cart';
+function App() {
+  const [showCart, setShowCart] = useState(false)
+const showCartHandler = ()=>{
+  
+    setShowCart(true)
+}
+const hideCartHandler = ()=>{
+  setShowCart(false)
+
+}
+const removeItem = (item,id)=>{
+  console.log('Nothing is impossible')
+}
+
+  return (
+    <CartProvider>
+      {showCart && <Cart onClose = {hideCartHandler}></Cart>}
+      {showCart && <Cart onClose = {hideCartHandler} ></Cart>}
+      <Header onCartClick={showCartHandler} />
+      <MealsSummary/>
+      <AvailableMeals/>
+      <AvailableMeals />
+
+    </CartProvider>
+  );
+}
+export default App;
---
+++ DIY-showing and enabling/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,33 @@
+import { useState,useContext } from 'react';
+import Input from '../../UI/Input';
+import classes from './MealItemForm.module.css';
+import CartContext from '../../../store/cart-context';
+const MealItemForm = (props) => {
+    const CartCtx = useContext(CartContext);
+    
+
+    const formSubmitHandler = (event)=>{
+        event.preventDefault();
+        CartCtx.addItem({amount:parseInt(event.target[0].value)});
+        CartCtx.addItem({...props.itemList,amount:Number(event.target[0].value)});
+
+
+    }
+  return (
+    <form className={classes.form} onSubmit={formSubmitHandler}>
+      <Input
+        label='Amount'
+        input={{
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
+      />
+      <button type='submit'>+ Add</button>
+    </form>
+  );
+};
+export default MealItemForm;
